008d4c99391e21dd271fa410c5794edda4e443f2
Amir <amir.dev21@gmail.com>
2021-09-17 21:46:38 -0400

    first commit, practice app Dr Rich

---
+++ Dr Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dr Rich
+//
+//  Created by Amir Noorafkan on 2020-05-21.
+//  Copyright © 2020 Amir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
